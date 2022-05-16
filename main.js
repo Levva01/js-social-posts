ba75c918fc0715a4c92fd12fcd5655e17f0dbfba
Levva01 <vellanifederico13@gmail.com>
2022-05-16 16:26:54 +0200

    milestone 2 completata

--- main.js
+++ main.js
@@ -55,3 +55,69 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+let postContainer = document.querySelector(".posts-list");
+
+for(let i = 0; i < posts.length; i++){
+
+    const post = `
+    
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${posts[i].author.image}" alt="">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${posts[i].author.name}</div>
+                    <div class="post-meta__time">${posts[i].created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${posts[i].content}</div>
+        <div class="post__image">
+            <img src="${posts[i].media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a onmouseover="likeClicked()" id="${i}" class="like-button  js-like-button" data-postid="${posts[i].id}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-${i}" class="js-likes-counter">${posts[i].likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>
+    
+    `
+
+    postContainer.innerHTML += post;
+
+}
+
+
+
+function likeClicked(){
+    
+    for(let i = 0; i < posts.length; i++){
+
+        let temp;
+
+        document.getElementById(i).addEventListener("click", function(){
+
+            document.getElementById(i).classList.add("like-button--liked");
+            
+            temp = posts[i].likes;
+            temp = temp + 1;
+
+            document.getElementById(`like-counter-${i}`).innerHTML = temp;
+
+        });
+
+    }
+
+}
